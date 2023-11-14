fd31c1df39ec445ca5ff9906530f413e1360593f
PETER IRUNGU <peteralidante254@gmail.com>
2023-11-14 11:15:16 +0300

    homepage style

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Drawer extends StatelessWidget {
+const Drawer({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
